72bce0d710c6b1d4603fc13138581c2591735fc2
Mattias Martens <mattias.martens@wealthbar.com>
2019-01-01 10:39:42 -0800

    remove unused part of doGet logic

--- src/do_get.ts
+++ src/do_get.ts
@@ -6,9 +6,6 @@ type GetRequest = {
 
 export function doGet(e: GetRequest) {
   let q = e.parameter["q"];
-  if (q === "dist") {
-    return HtmlService.createHtmlOutputFromFile("dist/client-bundle.js");
-  } else {
-    return HtmlService.createTemplateFromFile("index").evaluate();
-  }
+
+  return HtmlService.createTemplateFromFile("index").evaluate();
 }
