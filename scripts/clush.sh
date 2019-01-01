000cbe211a3010d1b7f1a7b373e34367d8791ef7
Mattias Martens <mattias.martens@wealthbar.com>
2019-01-01 10:34:12 -0800

    script to wrap client-side bundle in <script> tag

---
+++ scripts/clush.sh
@@ -0,0 +1,5 @@
+webpack --config webpack.config.js
+./scripts/wrap-in-script.sh ./clasp/dist/index.js.html
+cd clasp
+clasp push
+cd ..
--- scripts/wrap-in-script.sh
+++ scripts/wrap-in-script.sh
@@ -1,5 +1,8 @@
-echo 'line 1' > /tmp/$1
-cat $1 >> /tmp/$1
-echo '</script>' >> /mp/$1
-cp /tmp/$1 $1
-rm /tmp/$1
+mkdir -p tmp/$1
+rm -r tmp/$1
+touch tmp/$1
+echo '<script>' > tmp/$1
+cat $1 >> tmp/$1
+echo '</script>' >> tmp/$1
+cp tmp/$1 $1
+rm -r tmp
