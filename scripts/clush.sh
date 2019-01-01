82649830e51b04a119d62f744d0cb60d579cb0f2
Mattias Martens <mattias.martens@wealthbar.com>
2019-01-01 12:44:40 -0800

    No need for webpack build in clush script, so remove it

--- scripts/clush.sh
+++ scripts/clush.sh
@@ -1,5 +1,3 @@
-webpack --config webpack.config.js
-./scripts/wrap-in-script.sh ./clasp/dist/index.js.html
 cd clasp
 clasp push
 cd ..
--- scripts/wrap-in-script.sh
+++ scripts/wrap-in-script.sh
@@ -3,6 +3,6 @@ rm -r tmp/$1
 touch tmp/$1
 echo '<script>' > tmp/$1
 cat $1 >> tmp/$1
-echo '</script>' >> tmp/$1
+echo '\n</script>' >> tmp/$1
 cp tmp/$1 $1
 rm -r tmp
